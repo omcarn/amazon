1700778af27619c39ec1ac7f6fa83eefec96abab
Omkar Nilakhe <35950803+omcarn@users.noreply.github.com>
2025-03-16 20:44:49 +0530

    conversion func added

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,17 +9,19 @@
 
   document.addEventListener("DOMContentLoaded", (e) => {
 
+    let url = 'https://www.pink5x.com/TDW7331/2R26CKL/?sub1=amazon';
+
     let clickEvent = document.getElementById('topmost-parent').addEventListener('click', (topevent) => {
-      window.open('https://www.pink5x.com/TDW7331/2R26CKL/?sub1=amazon');
+      window.open(url);
+      gtag_report_conversion(url);
       logClick(topevent);
     }, { once: true });
-
-    
 
       const elements = document.querySelectorAll('.discountLink');
       elements.forEach(element => {
         element.addEventListener('click', function (clickevent) {
-          window.open('https://www.pink5x.com/TDW7331/2R26CKL/?sub1=amazon');
+          window.open(url);
+          gtag_report_conversion(url);
           logClick(clickevent);
         });
       });
